151b6bec47ccf7d566fca5994c49a326f213512a
Alexsandro Silveira <marcelo.abuchain@terceiroslinx.com.br>
2021-10-20 14:47:06 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { HashRouter, Route, Link } from "react-router-dom";
 
 function App() {
+
+  const Home = () => <div><h2>Home</h2></div>
+  const About = () => <div><h2>About</h2></div>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter basename='/'>
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+        <hr />
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </div>
+    </HashRouter>
   );
 }
 
